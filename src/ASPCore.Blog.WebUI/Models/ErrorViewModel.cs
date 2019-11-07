72163af2d7655bc6a1a2481c112c02fcff59a8ad
vlad806 <vlad.tabunov@outlook.com>
2019-11-07 18:06:38 +0300

    Initial commit

---
+++ src/ASPCore.Blog.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPCore.Blog.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
